2c231cfe37ec74bc67cdf17428e9e7ffab015cf0
huser0 <hugo10sergio@gmail.com>
2022-09-30 01:05:34 -0300

    finalizando apps

--- appAnuncioDeVagas/App.js
+++ appAnuncioDeVagas/App.js
@@ -11,21 +11,29 @@ class App extends Component{
        <ScrollView showsVerticalScrollIndicator={false}>
           <View style={styles.box1}>
             <Text style={styles.subtitulo}>Desenvolvedor .NET</Text>
-            <Text style={styles.descricao}>R$ 4.000,00</Text>
+            <Text style={styles.salario}>R$ 4.000,00</Text>
+            <Text style={styles.descricao}>Profissional com conhecimento em .NET, Sql Server, Api Rest Full, Angular</Text>
+            <Text style={styles.contato}>(+55) 9999-9999</Text>
           </View>
           <View style={styles.box2}>
             <Text style={styles.subtitulo}>Desenvolvedor Java</Text>
-            <Text>R$ 5.000,00</Text>
+            <Text style={styles.salario}>R$ 5.000,00</Text>
+            <Text style={styles.descricao}>Profissional com conhecimento em Java 11, Spring MVC, Apache Kafka, Kubernates, AWS S3</Text>
+            <Text style={styles.contato}>(+55) 9999-9999</Text>
+
           </View>
           <View style={styles.box3}>
             <Text style={styles.subtitulo}>Sofware Engineer</Text>
-            <Text>R$ 10.000,00</Text>
-            <Text></Text>
-            <Text>(+55) 9999-9999</Text>
+            <Text style={styles.salario}>R$ 10.000,00</Text>
+            <Text style={styles.descricao}>Conhecimento em Rust, Golang, React, Node, Integração CI/CD</Text>
+            <Text style={styles.contato}>(+55) 9999-9999</Text>
           </View>
           <View style={styles.box4}>
             <Text style={styles.subtitulo}>Scrum Master</Text>
-            <Text>R$ 5.000,00</Text>
+            <Text style={styles.salario}>R$ 5.000,00</Text>
+            <Text style={styles.descricao}>Conhecimento em metodologias ageis, facilitador de equipes, conhecimento em design thinking</Text>
+            <Text style={styles.contato}>(+55) 9999-9999</Text>
+
 
           </View>    
         </ScrollView>
@@ -45,20 +53,27 @@ const styles = StyleSheet.create({
       fontSize: 35,
       textAlign: 'center',
       color: `#b22222`,
+      fontWeight: 'bold'
       
   },
   subtitulo:{
-      margin: 10,
+      padding: 10,
       fontSize: 19,
-      textAlign: 'center',
       color: `#191970`,
-      
   },
+  salario:{
+      fontSize: 20,
+      paddingHorizontal: 10,
+      color: '#70f',
+      fontWeight: 'bold'
+  },  
   descricao:{
-      textAlign: 'center',
-      fontSize: 15,
-
-
+    padding: 10,
+    fontSize: 20,
+  },
+  contato: {
+    padding: 10,
+    fontSize: 20
   },
   box1:{
     backgroundColor: 'white',
@@ -67,6 +82,7 @@ const styles = StyleSheet.create({
     height: 250,
     margin: 5,
     borderWidth: 4,
+    borderRadius: 10
   },
   box2:{
     backgroundColor: 'white',
@@ -75,6 +91,8 @@ const styles = StyleSheet.create({
     height: 250,
     margin: 5,
     borderWidth: 4,
+    borderRadius: 10
+
   },
   box3:{
     backgroundColor: 'white',
@@ -83,6 +101,8 @@ const styles = StyleSheet.create({
     height: 250,
     margin: 5,
     borderWidth: 4,
+    borderRadius: 10
+
   },
   box4:{
     backgroundColor: 'white',
@@ -91,6 +111,7 @@ const styles = StyleSheet.create({
     height: 250,
     margin: 5,
     borderWidth: 4,
+    borderRadius: 10
   },
 })
  
